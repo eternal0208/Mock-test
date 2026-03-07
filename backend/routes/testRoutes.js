11e781a592483344277706683abd3a4535664244
Lisa <mylisa@AJ-mac-m2.local>
2026-03-07 12:35:27 +0530

    Update test routes, series page and dependencies, remove testStore and cacheMiddleware

--- backend/routes/testRoutes.js
+++ backend/routes/testRoutes.js
@@ -2,27 +2,20 @@ const express = require('express');
 const router = express.Router();
 const { createTest, updateTest, addQuestions, getAllTests, getTestById, submitTest } = require('../controllers/testController');
 const { protect, optionalProtect } = require('../middleware/authMiddleware');
-const { cacheMiddleware, clearCacheWithPrefix } = require('../middleware/cacheMiddleware');
 
-// Cache clearing helper middleware
-const clearTestCache = (req, res, next) => {
-    clearCacheWithPrefix('/api/tests');
-    next();
-};
-
-router.post('/', clearTestCache, createTest);
-router.put('/:id', protect, clearTestCache, updateTest);
-router.put('/:id/questions', clearTestCache, addQuestions);
+router.post('/', createTest);
+router.put('/:id', protect, updateTest);
+router.put('/:id/questions', addQuestions);
 
 // Public/Protected split: Ideally these should be protected to know the user's field
-router.get('/series', optionalProtect, cacheMiddleware(600), require('../controllers/testController').getAllSeries);
-router.get('/series/:id', protect, cacheMiddleware(300), require('../controllers/testController').getSeriesById);
-router.get('/', optionalProtect, cacheMiddleware(300), getAllTests);
-router.get('/:id', protect, cacheMiddleware(300), getTestById);
+router.get('/series', optionalProtect, require('../controllers/testController').getAllSeries);
+router.get('/series/:id', protect, require('../controllers/testController').getSeriesById);
+router.get('/', optionalProtect, getAllTests);
+router.get('/:id', protect, getTestById);
 
-router.post('/:id/submit', protect, clearTestCache, submitTest);
+router.post('/:id/submit', protect, submitTest);
 router.post('/:id/feedback', protect, require('../controllers/testController').submitFeedback);
-router.delete('/:id', clearTestCache, require('../controllers/testController').deleteTest);
+router.delete('/:id', require('../controllers/testController').deleteTest);
 
 router.get('/:id/analytics', protect, require('../controllers/testController').getTestAnalytics);
 router.put('/:id/visibility', require('../controllers/testController').toggleVisibility);
